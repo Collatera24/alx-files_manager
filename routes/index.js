fb10bb4964cc9b1aa81d2958dc27c3163795a20f
Collatera24 <kola.tella24@gmail.com>
2024-09-29 22:05:07 +0100

    files manager

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 
 const router = express.Router();
@@ -13,6 +14,7 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id/data', FilesController.getFile);
 
 
 export default router;
